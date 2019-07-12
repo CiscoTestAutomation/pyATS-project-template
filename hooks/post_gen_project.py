b500bd89ed400b272c6dc3044d557e90378f6b29
Sarah Li <xinyeli@cisco.com>
2019-07-12 10:53:10 -0400

    made some minor changes

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,1 +1,19 @@
-print("post gen hook")
+from jinja2 import Environment, FileSystemLoader
+import os
+
+# capture our current directory
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def generate_datafile():
+    src_file = "../{{ cookiecutter.project_name }}/data/template_datafile.yaml"
+    
+    include_datafile = input("include_datafile? (yes or no): ")
+    if include_datafile: # yes, include the datafile
+        filename = "{{ cookiecutter.datafile_name }}"
+        dest_file = "../{{ cookiecutter.project_name }}/data/" + filename + ".yaml"
+        os.rename(src_file, dest_file)
+    else: # no, don't include the datafile
+        os.remove(src_file)
+
+if __name__ == '__main__':
+    generate_datafile()
